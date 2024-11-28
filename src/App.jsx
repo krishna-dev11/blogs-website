cf3dbb3dd61f0dbb64516fcd46292255e01a73ef
krishna-dev11 <krishnagothwal0@gmail.com>
2024-11-28 18:19:29 +0530

    initiate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >App</div>
+  )export const baseUrl = "https://codehelp-apis.vercel.app/api/get-blogs";
+}
+
+export default App
